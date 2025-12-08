3deafd2f150cf108f189172c557b40ba5052db5d
Santosh Patel <santoshpatelvns5@gmail.com>
2025-12-08 13:37:24 +0530

    Add: Find user by ID API endpoint

---
+++ app/api/finduserbyID/[id]/route.js
@@ -0,0 +1,25 @@
+import { NextRequest, NextResponse } from "next/server";
+import { users } from "../../hello/route";
+
+export async function GET(request,{params}){
+    try {
+        const {id}=await params;
+        const user=users.find((p)=>p.id==parseInt(id));
+        if(!user){
+            return NextResponse.json({
+                message:"user not found",
+                sucess:false
+            },{status:404})
+        }
+        return NextResponse.json({
+            message: "User found successfully",
+            success: true,
+            user: user
+        }, { status: 200 });
+    } catch (error) {
+        return NextResponse.json({
+            message :"failed to find user",
+            sucess:false
+        },{status:500})
+    }
+}
