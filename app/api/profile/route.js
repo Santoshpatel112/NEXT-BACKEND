83a53837b8c8eef6450dcaa39de36b6e2baba163
Santosh Patel <santoshpatelvns5@gmail.com>
2025-12-09 13:40:05 +0530

    next headers

--- app/api/profile/route.js
+++ app/api/profile/route.js
@@ -1,13 +1,17 @@
 import {NextResponse } from "next/server"
-
+import { headers } from "next/headers"
 export async function GET(request){
 
     // to acess the header
-    const requestheader=new Headers(request.headers);
+    // const requestheader=new Headers(request.headers);
 
-    const authheader=requestheader.get("Authorization");
+    // const authheader=requestheader.get("Authorization");
 
-    console.log("authheader",authheader);
+    // console.log("authheader",authheader);
+
+    const headerlist=await headers();
+    const authheader=headerlist.get("Authorization");
+    console.log("authheader from next ",authheader);
     return NextResponse.json({
         data :"hello from the profile route",
         sucess :true
